70b56f622aa2cdbeef65981433a26a6fb81d92a4
Bruno Barbosa <bruno.barbosa@usemobile.xyz>
2021-02-18 19:23:08 -0300

    feat: rota de autenticação e calendar

--- routes/auth.js
+++ routes/auth.js
@@ -6,13 +6,6 @@ router.route('/').post((req, res) =>  {
     
     res.setHeader('Content-Type', 'application/json');
 
-    if (!req.body.login) {
-        res.end(JSON.stringify({ error: "missing login parameter" }));
-    }
-    else if (!req.body.password) {
-        res.end(JSON.stringify({ error: "missing password parameter" }));
-    } else {
-        
         var userName = req.body.login
         var userPass = req.body.password
 
@@ -27,9 +20,7 @@ router.route('/').post((req, res) =>  {
 
                     res.end(JSON.stringify({ user_name: user.name, user_img: user.img }));
             
-                } else {
-                    res.end(JSON.stringify({ message: "User incorreto" }));
-                }
+                } 
             });
           });
     }
