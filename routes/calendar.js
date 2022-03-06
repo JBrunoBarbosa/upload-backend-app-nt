a4f1dc83acafd9413e51cd1328462031021b3f10
bruno.barbosa <bruno.barbosa-ext@viavarejo.com.br>
2022-03-06 05:19:55 -0300

    sort events dates

--- routes/calendar.js
+++ routes/calendar.js
@@ -8,7 +8,7 @@ router.route('/').get((req, res) => {
 });
 
 router.route('/sorted').get((req, res) => {
-    Calendar.find().sort({when: 'descending'})
+    Calendar.find().sort({when: 'ascending'})
         .then(calendar => res.json(calendar))
         .catch(err => res.status(400).json('Error: ' + err))
 });
